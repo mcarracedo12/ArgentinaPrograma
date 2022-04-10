39ad9ec433dce1a8f71fe19e3b8b2855853b09b1
Marina <marinacarracedo14@gmail.com>
2022-04-10 18:19:24 -0300

    Agrego componente encabezado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './compenentes/encabezado/encabezado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent
   ],
   imports: [
     BrowserModule
