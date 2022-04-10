25b7aefb9a7026e4408ca1c38aa043072b751d28
Marina <marinacarracedo14@gmail.com>
2022-04-10 19:22:09 -0300

    Sin Errores

--- src/app/servicios/portfolio.service.ts
+++ src/app/servicios/portfolio.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PortfolioService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   obtenerDatos(){
     console.log("Bien !! Servicio ObtenerDatos esta corriendo")
