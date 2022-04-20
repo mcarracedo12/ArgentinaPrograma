729b6b486c56989331fc595c75a8b6215f293922
Marina <marinacarracedo14@gmail.com>
2022-04-19 21:22:37 -0300

    Experiencia y Formacion con Grid

--- src/app/componentes/educacion/educacion.component.ts
+++ src/app/componentes/educacion/educacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-educacion',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./educacion.component.css']
 })
 export class EducacionComponent implements OnInit {
+  educacionList:any;
 
-  constructor() { }
+  constructor(private obtenerDatos:PortfolioService) {}
 
   ngOnInit(): void {
+    this.obtenerDatos.obtenerDatos().subscribe(data=>{
+      console.log(data);
+    this.educacionList=data.formacion;
+  });
   }
 
 }
