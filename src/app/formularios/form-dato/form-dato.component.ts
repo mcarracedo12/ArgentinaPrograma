5844967b8b9af7bfeee451c298b455fdc965036e
Marina <marinacarracedo14@gmail.com>
2022-05-22 18:52:40 -0300

    formulario de actualizacion de datos ampliado

--- src/app/formularios/form-dato/form-dato.component.ts
+++ src/app/formularios/form-dato/form-dato.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-form-dato',
@@ -7,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormDatoComponent implements OnInit {
 
-  dato:string=""
-  constructor() { }
+  
+  infoData:any;
+
+  
+  constructor(private obtenerDatos:PortfolioService) { }
+
+  nombre:string="";
+  tituloactual:string="";
+  empresa:string="";
+  puesto:string="";
+  contacto:string="";
+  ciudad:string="";
+  provincia:string="";
+  pais:string="";
+
 
   ngOnInit(): void {
+    this.obtenerDatos.obtenerDatos().subscribe(data => {
+      // console.log(data);
+      this.infoData = data;
+      this.nombre=data.nombre;
+      this.tituloactual=data.tituloactual;
+      this.empresa=data.empresa;
+      this.puesto=data.puesto;
+      this.contacto=data.contacto;
+      this.ciudad=data.ciudad;
+      this.provincia=data.provincia;
+      this.pais=data.pais;
+    });
   }
 
   onEnviar(){
